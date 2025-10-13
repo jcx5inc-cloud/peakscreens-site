c76fc9f76d8a8e415cc5d6b3f315c22020b01da2
jcx5inc-cloud <jcx5inc@gmail.com>
2025-10-13 16:16:34 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // You can remove these two once everything is green.
+  typescript: { ignoreBuildErrors: false }, // set to true only if you need a quick pass
+  eslint: { ignoreDuringBuilds: true }      // avoids failing on lint warnings
+};
 export default nextConfig;
